df0ea61445678a6bda27f79c3f5c53350c9dd0ea
Kinoshita Kazuma <kazuma012023@gmail.com>
2026-09-17 16:56:32 +0900

    Add user-facing update log

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'canovia-shell-v30';
+const CACHE_VERSION = 'canovia-shell-v32';
 const META_CACHE = 'canovia-shell-meta-v1';
 const LAST_NETWORK_KEY = '/__canovia_last_network_success__';
 const LIKELY_SLEEP_AFTER_MS = 12 * 60 * 1000;
