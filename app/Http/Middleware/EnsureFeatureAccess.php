2ea1555c4def19dc49d8537732efe5d0f864989d
Kinoshita Kazuma <kazuma012023@gmail.com>
2026-09-23 13:38:18 +0900

    V40.6: pass Plan and Task scope into feature access

--- app/Http/Middleware/EnsureFeatureAccess.php
+++ app/Http/Middleware/EnsureFeatureAccess.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Enums\FeatureKey;
 use App\Services\FeatureAccessService;
 use Closure;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -19,9 +20,21 @@ final class EnsureFeatureAccess
         $featureKey = FeatureKey::tryFrom($feature);
         abort_unless($featureKey !== null, 500, 'Unknown Canovia feature key.');
 
-        $this->access->authorizeUse($request->user(), $featureKey, [
+        $context = [
             'route_name' => $request->route()?->getName(),
-        ]);
+        ];
+
+        foreach (['plan', 'task'] as $parameter) {
+            $value = $request->route($parameter);
+
+            if ($value instanceof Model) {
+                $context[$parameter.'_id'] = (int) $value->getKey();
+            } elseif (is_numeric($value)) {
+                $context[$parameter.'_id'] = (int) $value;
+            }
+        }
+
+        $this->access->authorizeUse($request->user(), $featureKey, $context);
 
         return $next($request);
     }
